dd074682c204576c0dc8f05e1b55830a7ee84c6a
Marius Zilinskas <mariuzmail@gmail.com>
2024-08-12 13:39:07 +0100

    creates schemas for People

---
+++ src/main/java/com/mariuszilinskas/vsp/contentservice/model/document/People.java
@@ -0,0 +1,37 @@
+package com.mariuszilinskas.vsp.contentservice.model.document;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
+import org.springframework.data.mongodb.core.mapping.MongoId;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@Document(collection = "people")
+public class People {
+
+    @MongoId
+    private String id;
+
+    @Indexed
+    @Field("name")
+    private String name;
+
+    @Field("description")
+    private String description;
+
+    @Field("born")
+    private LocalDate born;
+
+    @Field("died")
+    private LocalDate died;
+
+    @Field("imageURL")
+    private String imageURL;
+
+}
