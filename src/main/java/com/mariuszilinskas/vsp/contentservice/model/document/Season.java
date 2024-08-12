56a1d1ef7c0ae1ec6a135a9f4d0fcf45cddac3c4
Marius Zilinskas <mariuzmail@gmail.com>
2024-08-12 13:28:02 +0100

    creates schemas for Seasons and Episodes

---
+++ src/main/java/com/mariuszilinskas/vsp/contentservice/model/document/Season.java
@@ -0,0 +1,26 @@
+package com.mariuszilinskas.vsp.contentservice.model.document;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
+import org.springframework.data.mongodb.core.mapping.MongoId;
+
+@Getter
+@Setter
+@Document(collection = "seasons")
+public class Season {
+
+    @MongoId
+    private String id;
+
+    @Field("seriesId")
+    private String seriesId;
+
+    @Field("seasonNumber")
+    private int seasonNumber;
+
+    @Field("poster")
+    private String poster;
+
+}
