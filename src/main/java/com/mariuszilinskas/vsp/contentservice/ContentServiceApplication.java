35c42a3506ac8105b2ad638012b510fbedc9273b
Marius Zilinskas <mariuzmail@gmail.com>
2024-12-05 23:19:59 +0000

    adds caching to main application class

--- src/main/java/com/mariuszilinskas/vsp/contentservice/ContentServiceApplication.java
+++ src/main/java/com/mariuszilinskas/vsp/contentservice/ContentServiceApplication.java
@@ -2,10 +2,12 @@ package com.mariuszilinskas.vsp.contentservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 
 @RefreshScope
 @SpringBootApplication
+@EnableCaching
 public class ContentServiceApplication {
 
 	public static void main(String[] args) {
