864d20aff5e975f36793e20e68242bbb18e4ec13
Marius Zilinskas <mariuzmail@gmail.com>
2024-12-05 22:13:10 +0000

    updates SeasonAdminServiceImplTest

--- src/test/java/com/mariuszilinskas/vsp/contentservice/service/SeasonAdminServiceImplTest.java
+++ src/test/java/com/mariuszilinskas/vsp/contentservice/service/SeasonAdminServiceImplTest.java
@@ -25,9 +25,6 @@ public class SeasonAdminServiceImplTest {
     private SeasonRepository seasonRepository;
 
     @Mock
-    private MediaAdminService mediaAdminService;
-
-    @Mock
     private EpisodeAdminService episodeAdminService;
 
     @InjectMocks
@@ -59,9 +56,7 @@ public class SeasonAdminServiceImplTest {
         SeasonRequest request = new SeasonRequest(seasonNumber, 9.0, "poster_url");
 
         when(seasonRepository.existsBySeriesIdAndSeasonNumber(seriesId, seasonNumber)).thenReturn(false);
-        when(seasonRepository.countBySeriesId(seriesId)).thenReturn(1);
         when(seasonRepository.save(captor.capture())).thenReturn(season);
-        when(mediaAdminService.updateSeriesSeasonCount(seriesId, 2)).thenReturn(seasonNumber);
 
         // Act
         Season response = seasonAdminService.createSeasonForSeries(seriesId, request);
@@ -71,9 +66,7 @@ public class SeasonAdminServiceImplTest {
         assertEquals(season.getId(), response.getId());
 
         verify(seasonRepository, times(1)).existsBySeriesIdAndSeasonNumber(seriesId, seasonNumber);
-        verify(seasonRepository, times(1)).countBySeriesId(seriesId);
         verify(seasonRepository, times(1)).save(captor.capture());
-        verify(mediaAdminService, times(1)).updateSeriesSeasonCount(seriesId, seasonNumber);
 
         Season savedSeason = captor.getValue();
         assertEquals(seriesId, savedSeason.getSeriesId());
@@ -97,9 +90,7 @@ public class SeasonAdminServiceImplTest {
         // Assert
         verify(seasonRepository, times(1)).existsBySeriesIdAndSeasonNumber(seriesId, seasonNumber);
 
-        verify(seasonRepository, never()).countBySeriesId(seriesId);
         verify(seasonRepository, never()).save(any(Season.class));
-        verify(mediaAdminService, never()).updateSeriesSeasonCount(seriesId, seasonNumber);
     }
 
     // ------------------------------------
@@ -180,63 +171,19 @@ public class SeasonAdminServiceImplTest {
     // ------------------------------------
 
     @Test
-    void testUpdateSeasonEpisodeCount() {
-        // Arrange
-        ArgumentCaptor<Season> captor = ArgumentCaptor.forClass(Season.class);
-
-        when(seasonRepository.findByIdAndSeriesId(seasonId, seriesId)).thenReturn(Optional.of(season));
-        when(seasonRepository.save(captor.capture())).thenReturn(season);
-
-        // Act
-        int response = seasonAdminService.updateSeasonEpisodeCount(seriesId, seasonId, 0);
-
-        // Assert
-        verify(seasonRepository, times(1)).findByIdAndSeriesId(seasonId, seriesId);
-        verify(seasonRepository, times(1)).save(captor.capture());
-
-        assertEquals(0, response);
-        Season savedSeason = captor.getValue();
-        assertEquals(0, savedSeason.getEpisodeCount());
-    }
-
-    @Test
-    void testUpdateSeasonEpisodeCount_NegativeCountPassed_ZeroReturned() {
-        // Arrange
-        ArgumentCaptor<Season> captor = ArgumentCaptor.forClass(Season.class);
-
-        when(seasonRepository.findByIdAndSeriesId(seasonId, seriesId)).thenReturn(Optional.of(season));
-        when(seasonRepository.save(captor.capture())).thenReturn(season);
-
-        // Act
-        int response = seasonAdminService.updateSeasonEpisodeCount(seriesId, seasonId, -1);
-
-        // Assert
-        verify(seasonRepository, times(1)).findByIdAndSeriesId(seasonId, seriesId);
-        verify(seasonRepository, times(1)).save(captor.capture());
-
-        assertEquals(0, response);
-        Season savedSeason = captor.getValue();
-        assertEquals(0, savedSeason.getEpisodeCount());
-    }
-
-    // ------------------------------------
-
-    @Test
     void testRemoveSeasonFromSeries() {
         // Arrange
         when(seasonRepository.findByIdAndSeriesId(seasonId, seriesId)).thenReturn(Optional.of(season));
-        when(seasonRepository.countBySeriesId(seriesId)).thenReturn(1);
-        when(mediaAdminService.updateSeriesSeasonCount(seriesId, 0)).thenReturn(0);
         doNothing().when(episodeAdminService).removeAllEpisodesFromSeason(seriesId, seasonId);
+        doNothing().when(seasonRepository).delete(season);
 
         // Act
         seasonAdminService.removeSeasonFromSeries(seriesId, seasonId);
 
         // Assert
         verify(seasonRepository, times(1)).findByIdAndSeriesId(seasonId, seriesId);
-        verify(seasonRepository, times(1)).countBySeriesId(seriesId);
-        verify(mediaAdminService, times(1)).updateSeriesSeasonCount(seriesId, 0);
         verify(episodeAdminService, times(1)).removeAllEpisodesFromSeason(seriesId, seasonId);
+        verify(seasonRepository, times(1)).delete(season);
     }
 
     @Test
@@ -253,8 +200,8 @@ public class SeasonAdminServiceImplTest {
         verify(seasonRepository, times(1)).findByIdAndSeriesId(seasonId, seriesId);
 
         verify(seasonRepository, never()).countBySeriesId(anyString());
-        verify(mediaAdminService, never()).updateSeriesSeasonCount(anyString(), anyInt());
         verify(episodeAdminService, never()).removeAllEpisodesFromSeason(anyString(), anyString());
+        verify(seasonRepository, never()).delete(any(Season.class));
     }
 
     // ------------------------------------
@@ -262,15 +209,15 @@ public class SeasonAdminServiceImplTest {
     @Test
     void testRemoveAllSeasonsFromSeries() {
         // Arrange
-        doNothing().when(seasonRepository).deleteBySeriesId(seriesId);
         doNothing().when(episodeAdminService).removeAllEpisodesFromSeries(seriesId);
+        doNothing().when(seasonRepository).deleteBySeriesId(seriesId);
 
         // Act
         seasonAdminService.removeAllSeasonsFromSeries(seriesId);
 
         // Assert
-        verify(seasonRepository, times(1)).deleteBySeriesId(seriesId);
         verify(episodeAdminService, times(1)).removeAllEpisodesFromSeries(seriesId);
+        verify(seasonRepository, times(1)).deleteBySeriesId(seriesId);
     }
 
 }
